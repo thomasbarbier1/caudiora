8cf604b68fc0b697c66b49175b332c5e456fc1b1
thomas barbier <thomas.hugo.barbier@gmail.com>
2026-09-23 11:58:34 +0200

    Update some function names, added file copyright text, and doxygen file name header

--- src/processing.h
+++ src/processing.h
@@ -1,7 +1,23 @@
+/*
+ * Copyright (c) 2018, Thomas Barbier
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifndef CAUDIORA_PROCESSING_H
 #define CAUDIORA_PROCESSING_H
 
 int processing_init(void);
 int processing_stop(void);
-void* processing_start(void *ctx);
+void* processing_thread(void *ctx);
 #endif //CAUDIORA_PROCESSING_H
